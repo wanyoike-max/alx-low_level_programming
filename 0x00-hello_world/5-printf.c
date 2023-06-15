2e3bf77ceb4c93799b9899f21f6d3391d030e9c1
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-15 13:25:47 +0300

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,8 @@
  * but the outcome is a piece of art,
  * Return: 0(success)
  */
-
 int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
