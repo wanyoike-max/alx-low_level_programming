16df7e9db2e17e220f5dd4cf97861fea27a3de7b
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-15 02:57:27 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-printf("with proper grammar,but the outcome is a piece of art,\n");
-returrn(0);
+	printf("with proper grammar,but the outcome is a piece of art,\n");
+	returrn(0);
 }
