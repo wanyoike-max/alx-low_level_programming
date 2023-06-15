61ec842a58b9741f2a9b488ec784f7226278812a
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-15 01:26:02 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include <stdio.h> 
+ #include <stdio.h>
+ 
+ /**
+ * the main prints the phrase "Programmming is like building a multilingual puzzle"
+ * the programme should return 0 
+ * */
 
-int main(void)
-{
+ int main(void)
+ {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}
+ }  
