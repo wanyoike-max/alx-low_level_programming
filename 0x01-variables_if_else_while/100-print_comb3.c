49af207a58b8a16e835cf1ac32e565444d13d7d1
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-16 15:59:38 +0300

    print5

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 	int i;
 	int m;
 
-	for (i = 35; i < 45; i++)
+	for (i = 48; i < 58; i++)
 	{
-		for (m = 35; m < 45; m++)
+		for (m = 48; m < 58; m++)
 		{
 			if (i != m && i < m)
 			{
-				putchar(i + '0');
-				putchar(m + '0');
-				if (m == 44 && i == 43)
+				putchar(i);
+				putchar(m);
+				if (m == 57 && i == 56)
 				{
 					break
 					;
