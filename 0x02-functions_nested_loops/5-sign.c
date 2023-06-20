be9289d0e40e052d78ed1ef7ab1d208b33d18f24
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-20 13:40:48 +0300

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_sign - prints the sign of a number
- * @n - the int to check
+ * @n - the character to be tested 
  * Return: 1 and prints + if n > 0
  * 0 and prints 0 if n is zero
  * -1 and prints - if n < 0
