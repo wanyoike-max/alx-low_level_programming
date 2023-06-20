02fe48b1a6b34d313a640d5582c713b59e3045b8
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-20 15:14:41 +0300

    print_to_98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * print_to_98 - prints all numbers from n to 98 
- * followed by a new line 
- * @n: print from this number 
+ * print_to_98 - prints all numbers from n to 98
+ * followed by a new line
+ * @n: print from this number
  */
 
 void print_to_98(int n)
