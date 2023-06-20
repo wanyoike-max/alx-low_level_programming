620ca8aca043b53019b7260007a76f73dd464b7f
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-20 15:45:53 +0300

    times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -32,7 +32,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					-putchar((k / 10) + '0');
+					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 				else if (k >= 100)
