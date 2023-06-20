aafc9e92f783c28b52ba2f167d5e1bbf56737699
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-20 16:04:15 +0300

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-	unsigned long int dum3, sum5, sum;
+	unsigned long int sum3, sum5, sum;
 	int i;
 	
 	sum3 = 0;
@@ -21,7 +21,7 @@ int main(void)
 			sum3 = sum3 + i;
 		} else if ((i % 5) == 0)
 		{
-			sum5 = sum5 = i;
+			sum5 = sum5 + i;
 		}
 	}
 	sum = sum3 + sum5;
