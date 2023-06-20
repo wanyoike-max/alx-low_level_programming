3c7776b169a091bb6311ebbb6040f5fc15cdf534
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-20 12:38:03 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "mian.h"
+/**
+ * print_alphabet_x10 - prints 10 times the aplhabet in lowercase
+ * followed by a new line
+ * Return: 0(success)
+ */
+
+void print_alphabet_x10(void);
+{
+	char m;
+	int i;
+	
+	i = 0;
+	
+	while (i < 10)
+	{
+		m = 'a';
+		while (m <= 'z')
+		{
+			_putchar(m);
+			m++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
