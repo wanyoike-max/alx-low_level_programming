0953cd27c2d9f50e453c5e0c745476fafa4eb131
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-21 21:47:44 +0300

    task

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,7 @@
 */
 void print_remaining_days(int month, int day, int year)
 {
-    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
     {
         if (month > 2)
         {
@@ -27,3 +27,4 @@ void print_remaining_days(int month, int day, int year)
         printf("Remaining days: %d\n", 365 - day);
     }
 }
+
