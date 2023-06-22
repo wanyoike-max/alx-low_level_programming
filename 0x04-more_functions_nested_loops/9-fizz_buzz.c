32572d10b3aaf9f615414936508c2842a071be27
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-22 17:53:00 +0300

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,22 +10,31 @@
 
 int main(void)
 {
-int i;
+	int i;
 
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 == 0)
-printf("FizzBuzz");
-else if (i % 3 == 0)
-printf("Fizz");
-else if (i % 5 == 0)
-printf("Buzz");
-else
-printf("%d", i);
-
-if (i < 100)
-printf(" ");
-}
-printf("\n");
-return (0);
+	for (i = 1; i <= 1--; i++)
+	{
+		if ((i % 3 == 0) && (x % 5 == 0))
+		{
+			prntf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else 
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
 }
