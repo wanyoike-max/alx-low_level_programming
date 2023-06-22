bdeeeb630ded1260835de6b056e4dd0533c55c61
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-22 23:20:05 +0300

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,19 +14,19 @@ for (i = 1; i <= 100; i++)
 {
 if ((i % 3 == 0) && (i % 5 == 0))
 {
-printf("FizzBuzz");
+printf("FizzBuzz ");
 }
 else if (i % 3 == 0)
 {
-printf("Fizz");
+printf("Fizz ");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz");
+printf("Buzz ");
 }
 else
 {
-printf("%d", i);
+printf("%d ", i);
 }
 if (i != 100)
 {
@@ -35,4 +35,4 @@ printf(" ");
 }
 printf("\n");
 return (0);
-}
+i}
