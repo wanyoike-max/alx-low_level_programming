ad44f9d1a023e38cd34cad95841546ef8b21a437
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-22 12:28:54 +0300

    print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,24 +6,18 @@
 
 void print_diagonal(int n)
 {
-	if ( n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		int i; k;
+	int i, k;
 
-		for (i = 0; i < n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	for ( i = 0; i < n; i++)
+	{
+		for (k = 0; k < i; k++)
 		{
-			for (k = 0; k < n; k++)
-			{
-				if (k == i)
-					_putchar('\\');
-				else if (k < i)
-					_putchar(' ');
-			}
-			_putchar('\n');
+			_putchar(' ');
 		}
+
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
