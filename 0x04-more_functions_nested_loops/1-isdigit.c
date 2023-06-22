a182a9967137ec8944ac5e80aace301470c481a1
wanyoike-max <wanyoikeg71@gmail.com>
2023-06-22 11:09:45 +0300

    _isfigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _isdigit - checks if digit from 0 to 9
+ * @c: the integer to be checked
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,5 @@
 ## MORE FUNCTIONS AND NESTED LOOPS
 
 * Script 0 shows us how to check for uppercase letters returns 1 if c is uppercase 0 otherwise
+
+* Script 1 show how to know if a charcater is an integer from 0 to 9 
