4f7db938d53fd39947dfbb9ce2adfe143cd9d0b3
wanyoike-max <wanyoikeg71@gmail.com>
2023-07-17 12:03:25 +0300

    STRUCTUES TYPEDEF

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,11 @@
+## 0x0E. C - Structures, typedef
+
+* Script 0 shows us how to Define a new type struct dog with the following elements:
+
+name, type = char *
+age, type = float
+owner, type = char *
+
+* Script 1 shows us how to Write a function that initialize a variable of type struct dog
+
+Prototype: void init_dog(struct dog *d, char *name, float age, char *owner);
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - a dog's basic info
+ * @name: first member
+ * @age: second number
+ * @owner: third memeber
+ * Decription: longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
+#endif
