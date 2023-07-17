8d31bb8e4046194440c7c307a08ad0d07723747a
wanyoike-max <wanyoikeg71@gmail.com>
2023-07-17 12:17:02 +0300

    STUCTURES TYPEDEF CORRECTIONS

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints name of the file
- * Return: always 0(success)
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("%S\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
