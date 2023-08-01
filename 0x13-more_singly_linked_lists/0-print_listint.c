6755c6b6cfcb29b734d2890643352aeda1cba5ed
wanyoike-max <wanyoikeg71@gmail.com>
2023-08-01 20:04:39 +0300

    0-print correction

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all elements of a lined list
- * @h: linked list of type listsint_t to print
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
