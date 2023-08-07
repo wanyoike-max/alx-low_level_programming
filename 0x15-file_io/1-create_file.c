fff223941fe1b6dc1bbf7c476489954e19c39abb
wanyoike-max <wanyoikeg71@gmail.com>
2023-08-07 05:40:30 -0700

    1-create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
-	
+
 	if (filename == NULL)
 		return (-1);
 
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, len);
-	
+
 	if (o == -1 || w == -1)
 		return (-1);
 
