6fda1b39cbaea6cfe14e5b879b244932955350c4
wanyoike-max <wanyoikeg71@gmail.com>
2023-09-04 02:16:10 -0700

    Error correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,9 +10,10 @@
  * Return: NULL
  *         a/c - the actual number of bytes the function can read and print.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t a, b, c;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
@@ -22,18 +23,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 
-	a = open(filename, O_RDONLY);
-	b = read(o, buffer, letters);
-	c = write(STDOUT_FILENO, buffer, r);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	if (a == -1 || b == -1 || c == -1 || c != b)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
 	}
 
 	free(buffer);
-	close(a);
+	close(o);
 
-	return (c);
+	return (w);
 }
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,11 +4,12 @@
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
- * Return: If the function fails - -1.
+ * Return: -1 when function fails
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int i, j, len = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -19,13 +20,13 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	j = write(o, text_content, len);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	if (i == -1 || j == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(i);
+	close(o);
 
 	return (1);
 }
